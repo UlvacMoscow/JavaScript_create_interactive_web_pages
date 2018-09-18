1f3308b0f159ffb3203ec88dc612c1b642be7aec
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-09-19 00:13:42 +0300

    update gallery

--- Homework_1.1_gallery/index.js
+++ Homework_1.1_gallery/index.js
@@ -1,48 +1,26 @@
-/*onst prevPhoto = document.getElementById("prevPhoto");
-const nextPhoto = document.getElementById("nextPhoto");
+const nextSlide = document.getElementById('nextPhoto');
+const prevSlide = document.getElementById('prevPhoto');
+const currentSlide = document.getElementById('currentPhoto');
 
-
-prevPhoto.onclick =
-
-function prevPhoto()    {
-    var gallery = document.getElementById("article-gallery");
-    gallery.style.prevPhoto = -400 + "px";
-}
-
-function nextPhoto()    {
-
-}*/
-const current = document.getElementById('currentPhoto');
-const collection = ["i/breuer-building.jpg",
+const collectionSlide = ["i/breuer-building.jpg",
                     "i/guggenheim-museum.jpg",
                     "i/headquarters.jpg",
                     "i/IAC.jpg",
                     "i/new-museum.jpg"];
 
-let image = 0;
+let slide = 0;
 
-function showPhoto() {
-  /*if (this.id === 'prevPhoto')  {
-    if (image === 0) {
-		image = collection.length - 1;
-  	} else {
-  		image--;
-  	}
-    current.src = collection[image];
-  }
-  if (this.id === 'nextPhoto')  {
-    if (image === 0) {
-		image = collection.length - 1;
-	} else {
-		image++;
-	}
-    current.src = collection[image];
-  }*/
-  console.log(this.id);
+function showNextSlide() {
+  slide += 1;
+  if (slide == collectionSlide.length)  { slide = 0 };
+  currentSlide.src = collectionSlide[slide];
 };
 
+function showPrevSlide()  {
+  slide -= 1;
+  if (slide == -1)  {slide = collectionSlide.length - 1};
+  currentSlide.src = collectionSlide[slide];
+}
 
-for (const buttonId of ['prevPhoto', 'nextPhoto'])  {
-  const button = document.getElementById('buttonId');
-  button.onclick = showPhoto;
-};
+nextSlide.onclick = showNextSlide;
+prevSlide.onclick = showPrevSlide;
